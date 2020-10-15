746de1bc18a8f24f57773af010462b16d61b81d6
Blamak <vlasherassanchez@gmail.com>
2020-10-15 11:33:26 +0200

    Java fases 3 i 4

--- Java/src/Fase3App.java
+++ Java/src/Fase3App.java
@@ -1,4 +1,25 @@
+import javax.swing.JOptionPane;
 
 public class Fase3App {
 
+	public static void main(String[] args) {
+		int anyInici = 1948;
+		int interval = 4;
+
+		int anyNaixement = Integer.parseInt(JOptionPane.showInputDialog("Introdueix el teu any de naixement"));
+
+		for (int i = anyInici; i <= anyNaixement; i += interval) {
+			System.out.println(i);
+		}
+
+		boolean esAnyTraspas = (anyNaixement % 4 == 0 || anyNaixement % 400 == 0) && !(anyNaixement % 100 == 0);
+		String bixestSi = String.format("L'any %d és any de traspàs.", anyNaixement);
+		String bixestNo = String.format("L'any %d no és any de traspàs.", anyNaixement);
+
+		if (esAnyTraspas) {
+			System.out.println(bixestSi);
+		} else {
+			System.out.println(bixestNo);
+		}
+	}
 }
