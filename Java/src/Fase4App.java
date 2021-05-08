9b7b11a3c361607c52025b07ae788c8c6b9cc444
vicla <vicla@MSI.home>
2021-05-08 19:50:16 +0200

    minor change

--- Java/src/Fase4App.java
+++ Java/src/Fase4App.java
@@ -2,16 +2,16 @@ public class Fase4App {
 
 	public static void main(String[] args) {
 		
-		String nom           = "Víctor Lasheras Sánchez";
+		String nom = "Víctor Lasheras Sánchez";
 		String dataNaixement = "15/07/1974";
-		int anyNaixement     = Integer.parseInt(dataNaixement.substring(dataNaixement.length()-4));
+		int anyNaixement = Integer.parseInt(dataNaixement.substring(dataNaixement.length()-4));
 
 		System.out.printf("El meu nom és %s.%n", nom);
 		System.out.printf("Vaig néixer el %s.%n", dataNaixement);
 
 		boolean esAnyTraspas = (anyNaixement % 4 == 0 && anyNaixement % 100 != 0) || anyNaixement % 400 == 0;
-		String bixestSi      = "El meu any de naixement és de traspàs.";
-		String bixestNo      = "El meu any de naixement no és de traspàs.";
+		String bixestSi = "El meu any de naixement és de traspàs.";
+		String bixestNo = "El meu any de naixement no és de traspàs.";
 
 		if (esAnyTraspas) {
 			System.out.println(bixestSi);
